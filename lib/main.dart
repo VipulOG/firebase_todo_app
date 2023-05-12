56ca5bc28df0b263d4cb286fe290202f7c25f4a9
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-05-12 15:53:02 +0530

    feat: :sparkles: Add Crashlytics initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -7,11 +9,25 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
+  // Ensure the Flutter engine is properly initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
   // Initialize Firebase.
   // This is necessary for Firebase services (e.g. Firebase Auth) to work
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // if platform is not web, initialize Crashlytics
+  if (!kIsWeb) {
+    // Pass all uncaught errors from the framework to Crashlytics.
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
 
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
