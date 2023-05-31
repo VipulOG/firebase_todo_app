df3907a8e0af729637d7560cbcc7bc15392c305d
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-05-31 14:58:17 +0530

    Remove documentation

--- lib/main.dart
+++ lib/main.dart
@@ -9,36 +9,22 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
-  // Ensure the Flutter engine is properly initialized
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase.
-  // This is necessary for Firebase services (e.g. Firebase Auth) to work
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // if platform is not web, initialize Crashlytics
   if (!kIsWeb) {
-    // Pass all uncaught errors from the framework to Crashlytics.
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-    // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
     PlatformDispatcher.instance.onError = (error, stack) {
       FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
       return true;
     };
   }
 
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
   runApp(MyApp(settingsController: settingsController));
 }
