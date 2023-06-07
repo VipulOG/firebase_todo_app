405f9da807aa8cbbb6d61927c80dcdbf345ca328
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-06-07 18:39:40 +0530

    Add signin, signout and is_authenticated provider

---
+++ lib/src/auth/signin_provider.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'signin_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$signInHash() => r'217cf2c687342b4e45771dbb82c8312589516449';
+
+/// See also [signIn].
+@ProviderFor(signIn)
+final signInProvider = AutoDisposeFutureProvider<UserCredential>.internal(
+  signIn,
+  name: r'signInProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$signInHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SignInRef = AutoDisposeFutureProviderRef<UserCredential>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
